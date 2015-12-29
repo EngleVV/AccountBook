1963e21a486f170fec338636318dd8ed9a7fd2ba
EngleVV <892244214@qq.com>
2015-12-29 16:05:44 +0800

    添加按钮事件

--- src/com/example/myapp/activities/AddDetailActivity.java
+++ src/com/example/myapp/activities/AddDetailActivity.java
@@ -9,8 +9,6 @@ import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.text.Editable;
-import android.text.TextWatcher;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.view.inputmethod.InputMethodManager;
@@ -23,7 +21,6 @@ import android.widget.Toast;
 
 import com.example.myapp.R;
 import com.example.myapp.common.util.CalendarUtils;
-import com.example.myapp.common.util.StringUtils;
 import com.example.myapp.db.DetailDatabaseHelper;
 
 /**
--- src/com/example/myapp/fragments/FragmentAccountCenter.java
+++ src/com/example/myapp/fragments/FragmentAccountCenter.java
@@ -3,21 +3,21 @@
  */
 package com.example.myapp.fragments;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 import android.annotation.SuppressLint;
+import android.app.Dialog;
+import android.content.Context;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.View.OnClickListener;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
-import android.widget.SimpleAdapter;
+import android.widget.TextView;
 
 import com.example.myapp.R;
 import com.example.myapp.db.AccountDatabaseHelper;
@@ -29,19 +29,19 @@ import com.example.myapp.db.AccountDatabaseHelper;
 public class FragmentAccountCenter extends Fragment {
 
 	/** 条目的logo */
-	private int logos[] = new int[] { R.drawable.logo_pay_cash,
+	private final int logos[] = new int[] { R.drawable.logo_pay_cash,
 			R.drawable.logo_pay_bankcard, R.drawable.logo_pay_zhifubao,
 			R.drawable.logo_pay_weixin };
 
 	/** 条目标题名称 */
-	private String names[] = new String[] { "现    金", "银行卡", "支付宝", "微    信" };
+	private final String names[] = new String[] { "现    金", "银行卡", "支付宝", "微    信" };
 
 	/** 账户余额值 */
 	private String balanceAmount[] = new String[] { "111.11", "222.22",
 			"333.33", "444.44" };
 
 	/** 编辑按钮图标 */
-	private int modify = R.drawable.logo_pay_modify;
+	private final int modify = R.drawable.logo_pay_modify;
 
 	/** 数据库操作对象 */
 	private AccountDatabaseHelper dbHelper;
@@ -64,28 +64,98 @@ public class FragmentAccountCenter extends Fragment {
 					cursor.getDouble(cursor.getColumnIndex("amount")));
 		}
 
-		// 设置adapter的值
-		List<Map<String, Object>> listItems = new ArrayList<Map<String, Object>>();
-		for (int i = 0; i < logos.length; i++) {
-			Map<String, Object> item = new HashMap<String, Object>();
-			item.put("logo", logos[i]);
-			item.put("name", names[i]);
-			item.put("balanceAmount", balanceAmount[i]);
-			item.put("modify", modify);
-			listItems.add(item);
-		}
-
-		SimpleAdapter simpleAdapter = new SimpleAdapter(getActivity(),
-				listItems, R.layout.simple_item_account_list, new String[] {
-						"logo", "name", "balanceAmount", "modify" }, new int[] {
-						R.id.account_list_logo, R.id.account_list_name,
-						R.id.account_list_amount, R.id.account_list_modify });
+		AccountListAdapter accountListAdapter = new AccountListAdapter(
+				getActivity());
 
 		// 为listView设置simpleAdapter
 		ListView listViewAccountList = (ListView) rootView
 				.findViewById(R.id.account_list);
-		listViewAccountList.setAdapter(simpleAdapter);
+		listViewAccountList.setAdapter(accountListAdapter);
 
 		return rootView;
+	}
+
+	/**
+	 * 内部类
+	 */
+	public class AccountListAdapter extends BaseAdapter {
+		/** 获取视图 */
+		LayoutInflater inflater;
+
+		/**
+		 * 构造函数
+		 * 
+		 * @param context
+		 *            上下文
+		 */
+		public AccountListAdapter(Context context) {
+			inflater = LayoutInflater.from(context);
+		}
+
+		@Override
+		public int getCount() {
+			return names.length;
+		}
+
+		@Override
+		public Object getItem(int position) {
+			return null;
+		}
+
+		@Override
+		public long getItemId(int position) {
+			return 0;
+		}
+
+		@SuppressLint("InflateParams")
+		@Override
+		public View getView(final int position, View convertView,
+				ViewGroup parent) {
+			ViewHolder viewHolder;
+			if (null == convertView) {
+				viewHolder = new ViewHolder();
+				convertView = inflater.inflate(
+						R.layout.simple_item_account_list, null);
+				viewHolder.imageViewLogo = (ImageView) convertView
+						.findViewById(R.id.account_list_logo);
+				viewHolder.textViewName = (TextView) convertView
+						.findViewById(R.id.account_list_name);
+				viewHolder.textViewAmount = (TextView) convertView
+						.findViewById(R.id.account_list_amount);
+				viewHolder.imageViewModify = (ImageView) convertView
+						.findViewById(R.id.account_list_modify);
+				convertView.setTag(viewHolder);
+			} else {
+				viewHolder = (ViewHolder) convertView.getTag();
+			}
+
+			viewHolder.imageViewLogo.setImageResource(logos[position]);
+			viewHolder.textViewName.setText(names[position]);
+			viewHolder.textViewAmount.setText(balanceAmount[position]);
+			viewHolder.imageViewModify.setImageResource(modify);
+
+			// 给listView中的按钮绑定点击事件
+			viewHolder.imageViewModify
+					.setOnClickListener(new OnClickListener() {
+
+						@Override
+						public void onClick(View v) {
+							// TODO Auto-generated method stub
+							new Dialog(getActivity()).show();
+						}
+					});
+
+			return convertView;
+		}
+	}
+
+	/**
+	 * 方便获取每项中的内容
+	 */
+	public final class ViewHolder {
+		public ImageView imageViewLogo;
+		public TextView textViewName;
+		public TextView textViewAmount;
+		public ImageView imageViewModify;
 	}
 }
